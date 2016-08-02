1f74af32ae76c8d01b6e79c57914c5e1320731b7
Francisco Rodríguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2016-08-02 09:01:48 +0100

    Fix call to create_folds

--- mca/dataset/get_folds.m
+++ mca/dataset/get_folds.m
@@ -1,6 +1,6 @@
 function [train_set, test_set] = get_folds(condition)  
 
-  [train_set, test_set] = createFolds(condition);
+  [train_set, test_set] = create_folds(condition);
   train_set = find(train_set)';
   test_set = find(test_set)';
 
