51fd119fba636a5672ecf7c38d720e75d6a708f8
Francisco Rodríguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2016-10-23 11:29:35 +0100

    Further fix create results file names in filtering

--- util/save/save_fom.m
+++ util/save/save_fom.m
@@ -1,10 +1,10 @@
 function [ ] = save_fom(fom, intervention, condition, ...
     feat_set, filter_type)
 
-  if ((nargin < 4) || strcmp(filter_type, ''))
-    path = get_foms_path(intervention, condition, feat_set);
-  else
+  if ((nargin > 4) && ~strcmp(filter_type, ''))
     path = get_foms_path(intervention, condition, feat_set, filter_type);
+  else
+    path = get_foms_path(intervention, condition, feat_set);
   end
   
   save(path, 'fom');
--- util/save/save_predictions.m
+++ util/save/save_predictions.m
@@ -1,12 +1,12 @@
 function [ ] = save_predictions(preds, intervention, condition, ...
     feat_set, mode, filter_type)
 
-  if ((nargin < 6) || strcmp(filter_type, ''))
-    path = get_predictions_path(...
-          intervention, condition, feat_set, mode);
-  else
+  if ((nargin > 5) && ~strcmp(filter_type, ''))
     path = get_predictions_path(...
           intervention, condition, feat_set, mode, filter_type);
+  else
+    path = get_predictions_path(...
+          intervention, condition, feat_set, mode);
   end
     
   save(path, 'preds');
