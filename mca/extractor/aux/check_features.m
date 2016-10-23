898fa6e6b7a8e85369ebf36573e98355b9cf531b
Francisco Rodríguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2016-10-23 09:33:45 +0100

    Further fix in check_features: Allow no filter_type in call

--- mca/extractor/aux/check_features.m
+++ mca/extractor/aux/check_features.m
@@ -1,8 +1,11 @@
 function [found] = check_features(feat_set, version, filter_type)
 
-    if(nargin < 2)
-        version = 'original';
-        filter_type = '';
+    if nargin < 2
+      version = 'original';
+    end
+    
+    if nargin < 3
+      filter_type = '';
     end
 
     found = exist(get_features_path(feat_set, version, filter_type), 'file');    
