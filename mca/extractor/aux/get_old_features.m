8e8209a086ff374aace72a0faddae237f24a9751
Francisco Rodríguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2016-10-23 09:46:18 +0100

    Fix retrieve old features without indicating filter_type

--- mca/extractor/aux/get_old_features.m
+++ mca/extractor/aux/get_old_features.m
@@ -1,7 +1,11 @@
 function [features, indices, objects] = ...
     get_old_features(feat_set, version, filter_type)
 
-    path = get_features_path(feat_set, version, filter_type);
+    if (nargin < 3)
+      path = get_features_path(feat_set, version);
+    else
+      path = get_features_path(feat_set, version, filter_type);
+    end
     
     aux = load(path);
     features = aux.features;
