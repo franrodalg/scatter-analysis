bb6fcbc60a917cc6e7549da3465197114e73a4d7
Francisco Rodríguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2016-10-27 19:58:41 +0100

    Avoid show summary crash when no filtering intervention is requested

--- util/print/show_experiment.m
+++ util/print/show_experiment.m
@@ -3,7 +3,7 @@ function [] = show_experiment(intervention, condition, feat_set, filter_type)
   global INTERVENTIONS CONDITIONS FEAT_SETS FILTER_TYPES;
 
   display(['INTERVENTION: ', INTERVENTIONS.(intervention)]);
-  if ~(strcmp(filter_type, ''))
+  if ((nargin > 3) && (~strcmp(filter_type, '')))
       display(['FILTER_TYPE: ', FILTER_TYPES.(filter_type)]);
   end
   display(['PARTITIONING CONDITION: ', CONDITIONS.(condition)]);
